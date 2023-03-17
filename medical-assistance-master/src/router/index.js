57f91fdb5961ec8b9ac419097a2a479061c83dcc
DaEartutu <DaEartutu@medical>
2023-03-17 16:22:48 +0800

    DaEartutu commit

---
+++ medical-assistance-master/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import AppView from '@/views/AppView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: AppView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/:pathMatch(.*)',
+    redirect: '/login'
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ medical-assistance-master/src/views/AppView.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>主页面</h1>
+    {{ token }}
+</template>
+<script>
+import http from '@/util/http'
+import { ElNotification } from 'element-plus'
+export default {
+  data () {
+    return {
+      token: window.localStorage.getItem('token')
+    }
+  },
+  mounted () {
+    if (!this.token) { // 校验token
+      this.$router.push('/login')
+    } else {
+      http({
+        method: 'post',
+        url: '/my/userinfo',
+        headers: {
+          authorization: this.token
+        }
+      }).then(res => {
+        console.log(res)
+        if (res.data.status === 0) {
+          ElNotification({
+            title: 'Welcome',
+            message: res.data.data.username + ' 登录',
+            type: 'success'
+          })
+        } else {
+          ElNotification({
+            title: 'warning',
+            message: res.data.msg + '请重新登录',
+            type: 'warning'
+          })
+          this.$router.push('./login')
+        }
+      })
+    }
+  }
+}
+</script>
+<style>
+</style>
