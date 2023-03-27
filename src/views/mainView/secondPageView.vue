0226ded0dac8257e11f75cc7ee16f3ca8b30d57b
huashaoya <3250742495@qq.com>
2023-03-28 00:58:19 +0800

    引入echarts

--- src/views/mainView/secondPageView.vue
+++ src/views/mainView/secondPageView.vue
@@ -2,7 +2,33 @@
     <div class="body">
       <div class="main">
          <div class="item">
-            <div class="title"><h2>科室使用情况</h2></div>
+            <div class="title">
+               <h2>科室使用情况</h2>
+            </div>
+            <div class="content">
+               <div class="item">
+                  <div class="title">
+                     <h3>科室使用情况</h3>
+                  </div>
+               </div>
+               <div class="item">
+                  <div class="title">
+                     <h3>科室使用情况</h3>
+                  </div>
+               </div>
+               <div class="item">
+                  <div class="title">
+                     <h3>手术室使用情况</h3>
+                  </div>
+                  <div id="chart-1" class="chart"></div>
+               </div>
+               <div class="item">
+                  <div class="title">
+                     <h3>CT房使用情况</h3>
+                  </div>
+                  <div id="chart-2" class="chart"></div>
+               </div>
+            </div>
          </div>
       </div>
       <div class="second">
@@ -16,6 +42,47 @@
     </div>
  </template>
 <script>
+import * as echarts from 'echarts'
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    this.drawChart('chart-1')
+    this.drawChart('chart-2')
+  },
+  methods: {
+    drawChart (id) {
+      const chartDom = document.getElementById(id)
+      const myChart = echarts.init(chartDom)
+
+      const option = {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+              color: 'rgba(180, 180, 180, 0.2)'
+            }
+          }
+        ]
+      }
+
+      option && myChart.setOption(option)
+    }
+  }
+}
 </script>
 <style scoped lang="scss">
 .body{
@@ -37,6 +104,23 @@
             text-align: center;
             color:white;
             padding:5px;
+         }
+         .content{
+            display:flex;
+            flex-wrap: wrap;
+            height: 100%;
+            align-content: flex-start;
+            .item{
+               width:40%;
+               margin:0 5%;
+               height: 40%;
+               margin-top: 3%;
+               margin-bottom: 0;
+               .chart{
+                  width:100%;
+                  height: calc(100% - 25px);
+               }
+            }
          }
       }
    }
